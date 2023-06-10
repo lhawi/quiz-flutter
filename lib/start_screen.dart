8a0f1ca3e59df692e8839009505d2e018db51eb7
lhawi <lukmanhawi@gmail.com>
2023-06-10 07:52:18 +0700

    Update image to transparent

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,10 +9,8 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
-          ),
+          Image.asset('assets/images/quiz-logo.png',
+              width: 300, color: const Color.fromARGB(164, 255, 255, 255)),
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
